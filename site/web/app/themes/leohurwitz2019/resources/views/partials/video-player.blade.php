a93d663e76a3977692c55120aaddb5e8895f6a65
Francois Huyghe <francois@13milliseconds.com>
2020-04-06 15:04:39 -0500

    Thumbnails for Vimeo or Youtube

--- site/web/app/themes/leohurwitz2019/resources/views/partials/video-player.blade.php
+++ site/web/app/themes/leohurwitz2019/resources/views/partials/video-player.blade.php
@@ -1,25 +1,24 @@
 @php
-    // determine the video id
-    $parse = parse_url( $video_uri );
-    $clip_id = ltrim( $parse['path'],'/' );
 
-    //return thumbnail uri
-    $vimeo_api_uri = 'http://vimeo.com/api/v2/video/' . $clip_id . '.php';
-    $vimeo_response = wp_remote_get( $vimeo_api_uri );
-    if( is_wp_error( $vimeo_response ) ) {
-    echo $vimeo_response;
-    } else {
-    $vimeo_response = unserialize( $vimeo_response['body'] );
-    $vimeo_url = $vimeo_response[0]['thumbnail_large'];
-    }
+    if (strpos($video_uri, 'vimeo') !== false) {
+        // determine the video id
+        $parse = parse_url( $video_uri );
+        $clip_id = ltrim( $parse['path'],'/' );
 
-    if($vimeo_url) {
-        $thumbnail = $vimeo_url;
+        //return thumbnail uri
+        $vimeo_api_uri = 'http://vimeo.com/api/v2/video/' . $clip_id . '.php';
+        $vimeo_response = wp_remote_get( $vimeo_api_uri );
+        if( is_wp_error( $vimeo_response ) ) {
+        echo $vimeo_response;
+        } else {
+        $vimeo_response = unserialize( $vimeo_response['body'] );
+        $thumbnail = $vimeo_response[0]['thumbnail_large'];
+        }
     } else {
         $thumbnail = get_the_post_thumbnail_url($post->ID, 'medium');
     }
 @endphp
-{{ get_the_post_thumbnail($post->ID, 'medium') }}
+
 <div class="video-player" style="background-image: url({{ $thumbnail }})">
     <div class="video-btn"><i class="fas fa-play"></i></div>
     <div class="embed-container">
