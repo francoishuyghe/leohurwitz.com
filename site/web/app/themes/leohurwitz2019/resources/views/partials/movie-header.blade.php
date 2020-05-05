a84b329f67df0cc95b25794e01a065f935635559
Francois Huyghe <francois@13milliseconds.com>
2020-05-05 16:51:36 -0500

    Autoplay videos

--- site/web/app/themes/leohurwitz2019/resources/views/partials/movie-header.blade.php
+++ site/web/app/themes/leohurwitz2019/resources/views/partials/movie-header.blade.php
@@ -2,20 +2,12 @@
   @if($video_embed)
     <header class="movie-header">
       <div class="container">
-        <div class="movie-players">
-        @php $video_uri = get_field('film', FALSE, FALSE); @endphp
-        <div class="">
-          @include('partials/video-player')
-        </div>
+        @php $first_film = get_field('film', FALSE, FALSE); @endphp
+        @php $second_film = get_field('second_film', FALSE, FALSE);  @endphp
 
-        @php $video_uri = get_field('second_film', FALSE, FALSE);  @endphp
-        @if($video_uri)
-          <div class="hidden">
-            @php $video_embed = get_field('second_film');  @endphp
-            @include('partials/video-player')
-          </div>
-        </div>
+        @include('partials/video-player')
 
+        @if($second_film)
           <div class="selection">
                 <a class="active" data-movie="1">
                     {{ $data['first_video_title'] }}
