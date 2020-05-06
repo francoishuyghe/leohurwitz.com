2636ae39098c2560242515b8c6a814062223d8cd
Francois Huyghe <francois@13milliseconds.com>
2020-05-06 13:08:37 -0500

    Fotter edit

--- site/web/app/themes/leohurwitz2019/resources/views/partials/footer.blade.php
+++ site/web/app/themes/leohurwitz2019/resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="content-info">
   <div class="container text-center">
-    &copy; <?php echo date('Y'); ?> Leo Hurwitz - 
+    &copy; <?php echo date('Y'); ?> Thomas Hurwitz - 
     <a href="mailto:info@leohurwitz.com">info@leohurwitz.com</a>
   </div>
 </footer>
