d679745fe12fd875083ea2e610304d83e8f10894
Francois Huyghe <francois@13milliseconds.com>
2020-05-06 15:52:57 -0500

    Fixed movie footer links

--- site/web/app/themes/leohurwitz2019/resources/views/partials/single-news.blade.php
+++ site/web/app/themes/leohurwitz2019/resources/views/partials/single-news.blade.php
@@ -1,8 +1,10 @@
 <div class="single-news">
     @php $link = get_field('link') @endphp
     <h6>{{ the_date() }}</h3>
-    @if($link) <a href="{{ $link }}" target="_blank"> @endif
-    <h3>{{ the_title() }}</h3>
+        <h3>
+        @if($link) <a href="{{ $link }}" target="_blank"> @endif
+            {{ the_title() }}
     @if($link) </a> @endif
+    </h3>
     {{ the_content() }}
 </div>
