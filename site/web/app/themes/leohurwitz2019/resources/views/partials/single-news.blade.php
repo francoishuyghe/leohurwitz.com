521a8eae32c7333eecdd614555fcd77129903c28
Francois Huyghe <francois@13milliseconds.com>
2020-06-05 16:07:32 -0500

    Borders on News

--- site/web/app/themes/leohurwitz2019/resources/views/partials/single-news.blade.php
+++ site/web/app/themes/leohurwitz2019/resources/views/partials/single-news.blade.php
@@ -1,6 +1,6 @@
 <div class="single-news">
     @php $link = get_field('link') @endphp
-    <h6>{{ the_date() }}</h3>
+    <h6>{{ get_the_date() }}</h6>
         <h3>
         @if($link) <a href="{{ $link }}" target="_blank"> @endif
             {{ the_title() }}
